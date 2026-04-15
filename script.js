0ffd4670932a2d78ee022170b3e48c128cfb1ec3
alejandroE345 <alejandroencinas345@gmail.com>
2026-04-15 17:43:43 +0000

    complete

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function processForm(event) {
     console.log("click");
     // Prevent this function from reloading the page when the form is submitted
     event.preventDefault();
-    // TODO: Create a newUser object that has all the user's info from the form
+    // Create a newUser object that has all the user's info from the form
     let newUser =  {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
@@ -29,28 +29,31 @@ function processForm(event) {
     };
     console.log(newUser);
 
-    // TODO: Call the addUser function and pass the newUser object as a parameter
+    // Call the addUser function and pass the newUser object as a parameter
+    addUser(newUser);
 }
 
 function addUser(user) {
     // Create a div to hold the user's info
     let div = document.createElement("div");
-    // TODO: Set the background color of the div to the user's favorite color
-
+    // Set the background color of the div to the user's favorite color
+    div.style.backgroundColor = user.favColor;
     // Create a heading for the user's name
     let name = document.createElement("h2");
-    // TODO: Set the text using the object data
+    // Set the text using the object data
+    name.innerText = user.name;
     div.appendChild(name); // Add the heading to the div
-
+    
     // Create a paragraph for the user's email
     let email = document.createElement("p");
-    // TODO: Set the text using the object data
+    // Set the text using the object data
+    email.innerText = user.email;
     div.appendChild(email); // Add the paragraph to the div
     
     // Create a paragrapn for the user's birthdate
     let birthdate = document.createElement("p");
-    // TODO: Set the text using the object data
+    // Set the text using the object data
+    birthdate.innerText = user.birthdate;
     div.appendChild(birthdate); // Add the paragraph to the div
-
     document.body.appendChild(div); // Add the div to the page
 }
